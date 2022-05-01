f611daf7816e93ff5acafb05ea2506d613537f9f
Sultan <godsultan42@gmail.com>
2022-05-01 22:55:02 +0200

    -Added animated svg in main page

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import './index.scss';
+
 import TextBlock from '../TextBlock';
 import Line from '../Line';
 import Button from '../Button';
 import { Link } from 'react-router-dom';
+import AnimatedSvg from '../AnimatedSvg';
 
 const Main = () => {
     return (
-        <div className="main">
-            <TextBlock style={{ paddingTop: '20%' }}>
-                <Line lettersArr={"Greetings".split('')} />
-                <Line lettersArr={"I'm Sultan,".split('')} />
-                <Line lettersArr={"Web Developer".split('')}/>
-            </TextBlock>
-            <Link to="/contact">
-                <Button color={"#f6ebcb"} hoverColor={"#252d2d"}>
-                    Contact me!
-                </Button>
-            </Link>
+        <div className="main main-flex">
+            <div>
+                <TextBlock style={{ paddingTop: '20%' }}>
+                    <Line lettersArr={"Greetings".split('')} />
+                    <Line lettersArr={"I'm Sultan,".split('')} />
+                    <Line lettersArr={"Web Developer".split('')} />
+                </TextBlock>
+                <Link to="/contact">
+                    <Button color={"#f6ebcb"} hoverColor={"#252d2d"}>
+                        Contact me!
+                    </Button>
+                </Link>
+            </div>
+            <AnimatedSvg />
         </div>
     );
 };
